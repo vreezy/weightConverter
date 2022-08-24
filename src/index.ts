e49565fbcc37703d8c310a68682803863e549684
vreezy <vreezy117@googlemail.com>
2022-08-24 15:16:00 +0200

    runs

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,43 @@
-import ExcelJS from 'exceljs';
+import Excel from 'exceljs';
+const fs = require('fs');
 
+interface IData {
+    datetime: string;
+    weight: number;
+}
+
+const load = async () => {
+    const workbook = new Excel.Workbook();
+    try {
+        await workbook.xlsx.readFile("Kurz Tagebuch mit messwerten.xlsx");
+
+    }
+    catch(exception) {
+        console.log(exception);
+    }
+
+    const worksheet = workbook.getWorksheet(1);
+    const rows = worksheet.getRows(2, 500);
+
+    const output: IData[] = [];
+
+    rows?.forEach(row => {
+        const datetime = row.getCell(1).value;
+        const weight = row.getCell(3).value;
+
+        if(datetime && weight) {
+            console.log(datetime, weight)
+            output.push({
+                datetime,
+                weight
+            } as IData)
+        }
+
+        let data = JSON.stringify(output);
+        fs.writeFileSync('output.json', data);
+    })
+    
+    
+}
+
+load();
